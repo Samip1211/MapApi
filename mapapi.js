2d48965b3954500d5056ddaeceb55db15bb19565
Samip Kothari <samipkothari@Samips-MacBook-Pro.local>
2017-04-12 20:36:07 -0500

    Changing package.json

--- mapapi.js
+++ mapapi.js
@@ -18,7 +18,7 @@ app.get('/listUsers', function (req, res) {
 	
 })
 
-var server = app.listen(process.env.PORT, function () {
+var server = app.listen(8080, function () {
 
   var host = server.address().address
   var port = server.address().port
