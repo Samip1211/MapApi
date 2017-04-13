42dd4c14588008062660a01f04e36f32b13b93a7
Samip Kothari <samipkothari@Samips-MacBook-Pro.local>
2017-04-13 15:05:54 -0500

    Daniel's function

--- mapapi.js
+++ mapapi.js
@@ -24,6 +24,32 @@ app.get('/getRestaurant/:latittude/:longitude', function (req, res) {
 	
 })
 
+app.get('/getRestaurantDetails/:id',function(req,res){
+	
+	http.get("https://maps.googleapis.com/maps/api/place/details/json?placeid="+req.params.id+"&key=AIzaSyAQvpmdy7gi3VVHuG0hnR0dRaU31MjtQas").then(function(response){
+	    res.header('Access-Control-Allow-Origin', '*');
+	     res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+	     res.header('Access-Control-Allow-Headers', 'accept, content-type, x-parse-application-id, x-parse-rest-api-key, x-parse-session-token');
+		 
+		 res.end(response.body);
+	});
+		
+})
+
+app.get('/getRestaurantId/:name/:city/:street/:state',function(req,res){
+	
+	http.get("https://maps.googleapis.com/maps/api/place/textsearch/json?query="+req.params.name+"+"+
+																				+req.params.city+"+"+
+																				+req.params.street+"+"+
+																				+req.params.state+).then(function(response){
+	    res.header('Access-Control-Allow-Origin', '*');
+	     res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+	     res.header('Access-Control-Allow-Headers', 'accept, content-type, x-parse-application-id, x-parse-rest-api-key, x-parse-session-token');
+		 
+		 res.end(response.body);
+	});
+})
+
 var server = app.listen(process.env.PORT, function () {
 
   var host = server.address().address
@@ -31,11 +57,4 @@ var server = app.listen(process.env.PORT, function () {
 
   console.log("Example app listening at http://%s:%s", host, port)
 
-})/*
-http.get("https://maps.googleapis.com/maps/api/place/details/json?placeid=ChIJ_UypeeMuTIYRGtrKERCRj2U&key=AIzaSyAQvpmdy7gi3VVHuG0hnR0dRaU31MjtQas").then(function(response) {
-    // Get the response body (JSON parsed - JSON response or jQuery object in case of XML response)
-    console.log(response.getBody());
-	
-    // Get the response raw body
-    console.log(response.body);
-});*/
+})
