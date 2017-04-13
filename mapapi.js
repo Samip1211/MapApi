aa502e6a401e8721735e7b0c2eb5307c7e43b3cf
Samip Kothari <samipkothari@Samips-MacBook-Pro.local>
2017-04-12 20:20:05 -0500

    Initial commit

---
+++ mapapi.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+
+var http = require("requestify");
+
+app.get('/listUsers', function (req, res) {
+	var reponseObject;
+	http.get("https://maps.googleapis.com/maps/api/place/details/json?placeid=ChIJ_UypeeMuTIYRGtrKERCRj2U&key=AIzaSyAQvpmdy7gi3VVH	uG0hnR0dRaU31MjtQas").then(function(response) {
+	    // Get the response body (JSON parsed - JSON response or jQuery object in case of XML response)
+	    console.log(response.getBody());
+		res.end(response.body);
+	    // Get the response raw body
+	    console.log(response.body);
+		
+	});
+	
+})
+
+var server = app.listen(process.env.port, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("Example app listening at http://%s:%s", host, port)
+
+})/*
+http.get("https://maps.googleapis.com/maps/api/place/details/json?placeid=ChIJ_UypeeMuTIYRGtrKERCRj2U&key=AIzaSyAQvpmdy7gi3VVHuG0hnR0dRaU31MjtQas").then(function(response) {
+    // Get the response body (JSON parsed - JSON response or jQuery object in case of XML response)
+    console.log(response.getBody());
+	
+    // Get the response raw body
+    console.log(response.body);
+});*/
